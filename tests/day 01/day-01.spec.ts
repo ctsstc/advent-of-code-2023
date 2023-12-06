57ba1ddec5a63b68f99ddde5a145a86380fac215
Cody Swartz <ctsstc@gmail.com>
2023-12-05 19:28:26 -0800

    Solve day 1 input

--- tests/day 01/day-01.spec.ts
+++ tests/day 01/day-01.spec.ts
@@ -1,6 +1,10 @@
 import { expect, test } from 'bun:test'
 import { solveDay01 } from './day-01'
 
-test('example',async () => {
-  expect(await solveDay01("tests/day 01/example.txt")).toBe(142)
-})
+test("example.txt", async () => {
+  expect(await solveDay01("tests/day 01/example.txt")).toBe(142);
+});
+
+test("input.txt", async () => {
+  expect(await solveDay01("tests/day 01/input.txt")).toBe(55712);
+});
