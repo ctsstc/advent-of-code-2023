fc58608f16950aff724dac3732c83ee5182f8939
Cody Swartz <ctsstc@gmail.com>
2023-12-11 03:31:08 -0800

    Solve Day 2 Part 1

--- tests/day-02/part-1/day-02-part-1.spec.ts
+++ tests/day-02/part-1/day-02-part-1.spec.ts
@@ -1,14 +1,20 @@
-import { describe, expect, test } from 'bun:test'
-import { SolveDay02Part1 } from './day-02-part-1'
+import { describe, expect, test } from "bun:test";
+import { Day02Part01Config, SolveDay02Part1 } from "./day-02-part-1";
 
 describe("Day 02 Part 1", () => {
+  const config: Day02Part01Config = {
+    maxRed: 12,
+    maxGreen: 13,
+    maxBlue: 14,
+  };
+
   test("Example", async () => {
     const file = import.meta.resolveSync("./day-02-part-1-example.txt");
-    expect(await SolveDay02Part1(file)).toBe(123);
+    expect(await SolveDay02Part1(file, config)).toBe(8);
   });
 
   test("Input", async () => {
     const file = import.meta.resolveSync("./day-02-part-1-input.txt");
-    expect(await SolveDay02Part1(file)).toBe(123);
+    expect(await SolveDay02Part1(file, config)).toBe(2348);
   });
 });
--- tests/day-02/part-1/day-02-part-1.ts
+++ tests/day-02/part-1/day-02-part-1.ts
@@ -1,7 +1,37 @@
 import { getLines } from "../../utilities/files";
 
-export async function SolveDay02Part1(filePath: string): Promise<number> {
+export interface Day02Part01Config {
+  maxRed: number;
+  maxGreen: number;
+  maxBlue: number;
+}
+
+export async function SolveDay02Part1(
+  filePath: string,
+  { maxRed, maxGreen, maxBlue }: Day02Part01Config
+): Promise<number> {
   const lines = await getLines(filePath);
 
-  return 0;
+  return lines.reduce((sum, line, idx) => {
+    const [_prefix, game] = line.split(": ");
+    const subsets = game.split("; ");
+
+    const allSubsetsValid = subsets.every((subsets) => {
+      const [_foundRed, redCount] = /(\d+) red/.exec(subsets) ?? [0, 0];
+      const [_foundGreen, greenCount] = /(\d+) green/.exec(subsets) ?? [0, 0];
+      const [_foundBlue, blueCount] = /(\d+) blue/.exec(subsets) ?? [0, 0];
+
+      return (
+        Number(redCount) <= maxRed &&
+        Number(greenCount) <= maxGreen &&
+        Number(blueCount) <= maxBlue
+      );
+    });
+
+    if (allSubsetsValid) {
+      sum += idx + 1;
+    }
+
+    return sum;
+  }, 0);
 }
