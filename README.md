2c5629dea390cb28fc51b5f6f043af5533ac1332
Cody Swartz <ctsstc@gmail.com>
2023-12-11 02:41:45 -0800

    Add day plop day generator script and templates
    Updated README to reflect changes and added the test scripts to it too.

--- README.md
+++ README.md
@@ -1,15 +1,28 @@
 # advent-of-code-2023
 
+## Dependencies
+
 To install dependencies:
 
 ```bash
 bun install
 ```
 
+## Running
+
 To run:
 
 ```bash
-bun run index.ts
+bun run test
+bun run test:watch
 ```
+
+## Creating a New Day
+
+```bash
+bunx plop
+```
+
+## Bun Stuff
 
 This project was created using `bun init` in bun v1.0.15. [Bun](https://bun.sh) is a fast all-in-one JavaScript runtime.
---
+++ plopfile.mjs
@@ -0,0 +1,65 @@
+/**
+ *
+ * @export
+ * @param {import('plop').NodePlopAPI} plop
+ */
+export default function (plop) {
+  // controller generator
+  plop.setGenerator("New Day", {
+    description: "Start a new AoC day",
+    prompts: [
+      {
+        type: "input",
+        name: "dayNumber",
+        message: "Day number:",
+      },
+    ],
+    actions: ({ dayNumber }) => {
+      const actions = [];
+      const dayRoot = `tests/day-${dayNumber}`;
+
+      for (const partNumber of [1, 2]) {
+        const partRoot = `${dayRoot}/part-${partNumber}`;
+        const baseFileName = `day-${dayNumber}-part-${partNumber}`;
+        const problemFileName = `${partRoot}/${baseFileName}`;
+        const exampleFileName = `${baseFileName}-example.txt`;
+        const inputFileName = `${baseFileName}-input.txt`;
+        const methodName = `SolveDay${dayNumber}Part${partNumber}`;
+
+        actions.push(
+          {
+            type: "add",
+            path: `${problemFileName}.ts`,
+            templateFile: "templates/new-day-source.hbs",
+            data: { partNumber, methodName },
+          },
+          {
+            type: "add",
+            path: `${problemFileName}.spec.ts`,
+            templateFile: "templates/new-day-specs.hbs",
+            data: {
+              partNumber,
+              baseFileName,
+              problemFileName,
+              exampleFileName,
+              inputFileName,
+              methodName,
+            },
+          },
+          {
+            type: "add",
+            path: `${partRoot}/${exampleFileName}`,
+            data: { partNumber },
+          },
+          {
+            type: "add",
+            path: `${partRoot}/${inputFileName}`,
+            data: { partNumber },
+          }
+        );
+      }
+
+      return actions;
+    },
+  });
+}
