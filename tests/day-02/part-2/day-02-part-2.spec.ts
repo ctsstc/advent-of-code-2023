7c6472a0fd7e2ebbab0851508c9ba59006ea37a7
Cody Swartz <ctsstc@gmail.com>
2023-12-11 03:45:47 -0800

    Solve Day 2 Part 2

--- tests/day-02/part-2/day-02-part-2.spec.ts
+++ tests/day-02/part-2/day-02-part-2.spec.ts
@@ -1,14 +1,14 @@
-import { describe, expect, test } from 'bun:test'
-import { SolveDay02Part2 } from './day-02-part-2'
+import { describe, expect, test } from "bun:test";
+import { SolveDay02Part2 } from "./day-02-part-2";
 
 describe("Day 02 Part 2", () => {
   test("Example", async () => {
     const file = import.meta.resolveSync("./day-02-part-2-example.txt");
-    expect(await SolveDay02Part2(file)).toBe(123);
+    expect(await SolveDay02Part2(file)).toBe(2286);
   });
 
   test("Input", async () => {
     const file = import.meta.resolveSync("./day-02-part-2-input.txt");
-    expect(await SolveDay02Part2(file)).toBe(123);
+    expect(await SolveDay02Part2(file)).toBe(76008);
   });
 });
--- tests/day-02/part-2/day-02-part-2.ts
+++ tests/day-02/part-2/day-02-part-2.ts
@@ -1,8 +1,29 @@
-import { getLines } from "../utilities/files";
+import { getLines } from "../../utilities/files";
 
 export async function SolveDay02Part2(filePath: string): Promise<number> {
-    const lines = await getLines(filePath);
+  const lines = await getLines(filePath);
 
-    return 0;
+  return lines.reduce((sum, line) => {
+    const [_prefix, game] = line.split(": ");
+    const subsets = game.split("; ");
+
+    const minimumSubset = subsets.reduce(
+      ({ red, green, blue }, subsets) => {
+        const [_foundRed, redCount] = /(\d+) red/.exec(subsets) ?? [0, 0];
+        const [_foundGreen, greenCount] = /(\d+) green/.exec(subsets) ?? [0, 0];
+        const [_foundBlue, blueCount] = /(\d+) blue/.exec(subsets) ?? [0, 0];
+
+        return {
+          red: Math.max(Number(redCount), red),
+          green: Math.max(Number(greenCount), green),
+          blue: Math.max(Number(blueCount), blue),
+        };
+      },
+      { red: 0, green: 0, blue: 0 }
+    );
+
+    const power = minimumSubset.red * minimumSubset.green * minimumSubset.blue;
+
+    return sum + power;
+  }, 0);
 }
-
