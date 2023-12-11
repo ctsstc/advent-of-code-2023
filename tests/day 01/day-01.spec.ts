1623c9b67af51beb5ab4d4dadb184f7b51ec5892
Cody Swartz <ctsstc@gmail.com>
2023-12-11 02:05:38 -0800

    Utilize relevant file io in day 1 tests

--- tests/day 01/day-01.spec.ts
+++ tests/day 01/day-01.spec.ts
@@ -1,22 +1,30 @@
-import { describe, expect, test } from 'bun:test'
-import { solveDay01, solveDay01Part2 } from './day-01'
+import { describe, expect, test } from "bun:test";
+import { solveDay01, solveDay01Part2 } from "./day-01";
 
 describe("Part 1", () => {
   test("example.txt", async () => {
-    expect(await solveDay01("tests/day 01/example.txt")).toBe(142);
+    expect(await solveDay01(import.meta.resolveSync("./example.txt"))).toBe(
+      142
+    );
   });
 
   test("input.txt", async () => {
-    expect(await solveDay01("tests/day 01/input.txt")).toBe(55712);
+    expect(await solveDay01(import.meta.resolveSync("./input.txt"))).toBe(
+      55712
+    );
   });
 });
 
 describe("Part 2", () => {
   test("example-part-2.txt", async () => {
-    expect(await solveDay01Part2("tests/day 01/example-part-2.txt")).toBe(281);
+    expect(
+      await solveDay01Part2(import.meta.resolveSync("./example-part-2.txt"))
+    ).toBe(281);
   });
 
   test("input.txt", async () => {
-    expect(await solveDay01Part2("tests/day 01/input.txt")).toBe(55413);
+    expect(await solveDay01Part2(import.meta.resolveSync("./input.txt"))).toBe(
+      55413
+    );
   });
 });
