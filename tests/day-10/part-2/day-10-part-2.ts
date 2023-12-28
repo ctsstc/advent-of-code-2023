e15df4c904dd2419c6998ea05cbb600da92f0722
Cody Swartz <ctsstc@gmail.com>
2023-12-27 23:09:26 -0800

    Start Day 10

---
+++ tests/day-10/part-2/day-10-part-2.ts
@@ -0,0 +1,8 @@
+import { getLines } from "../../utilities/files";
+
+export async function SolveDay10Part2(filePath: string): Promise<number> {
+    const lines = await getLines(filePath);
+
+    return 0;
+}
+
