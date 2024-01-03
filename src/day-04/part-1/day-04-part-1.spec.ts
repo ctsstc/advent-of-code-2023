573b46bb8a91d711add4786b14b6c476b404187d
Cody Swartz <ctsstc@gmail.com>
2024-01-03 01:01:51 -0800

    Add day 04 example

--- src/day-04/part-1/day-04-part-1.spec.ts
+++ src/day-04/part-1/day-04-part-1.spec.ts
@@ -1,10 +1,10 @@
-import { describe, expect, test } from 'bun:test'
-import { SolveDay04Part1 } from './day-04-part-1'
+import { describe, expect, test } from "bun:test";
+import { SolveDay04Part1 } from "./day-04-part-1";
 
 describe("Day 04 Part 1", () => {
   test("Example", async () => {
     const file = import.meta.resolveSync("./day-04-part-1-example.txt");
-    expect(await SolveDay04Part1(file)).toBe(123);
+    expect(await SolveDay04Part1(file)).toBe(13);
   });
 
   test("Input", async () => {
