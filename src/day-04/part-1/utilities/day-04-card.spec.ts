540ef2b281dcdf31cb86b92c95e6f8a1442134de
Cody Swartz <ctsstc@gmail.com>
2024-01-03 01:46:29 -0800

    Add failing test for card scores
    They seem to be returning 0.5 instead of 0 for the last two card's scores

--- src/day-04/part-1/utilities/day-04-card.spec.ts
+++ src/day-04/part-1/utilities/day-04-card.spec.ts
@@ -1,9 +1,14 @@
 import { describe, expect, it } from "bun:test";
+import { getLines } from "../../../utilities/files";
 import { day04ParseCard, day04ScoreCard } from "./day-04-card";
 
-describe("Day 04 Part 1 > Utility - Card", () => {
+describe("Day 04 Part 1 > Utility - Card", async () => {
   const cardExample = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53";
   const card = day04ParseCard(cardExample);
+
+  const cardsExample = import.meta.resolveSync("../day-04-part-1-example.txt");
+  const cardsLines = await getLines(cardsExample);
+  const cards = cardsLines.map((cardLine) => day04ParseCard(cardLine));
 
   describe("#day04ParseCard", () => {
     it("returns the card", () => {
@@ -30,6 +35,14 @@ describe("Day 04 Part 1 > Utility - Card", () => {
   describe("#day04ScoreCard", () => {
     it("scores the card", () => {
       expect(day04ScoreCard(card)).toEqual(8);
+    });
+
+    describe("card collection", () => {
+      const cardsScores = cards.map((card) => day04ScoreCard(card));
+
+      it("returns the expected scoring for the cards", () => {
+        expect(cardsScores).toEqual([8, 2, 2, 1, 0, 0]);
+      });
     });
   });
 });
