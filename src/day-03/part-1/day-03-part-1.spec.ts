4f1ff0abb85c59e204ca1a3edea041412b757ef3
Cody Swartz <ctsstc@gmail.com>
2024-01-03 17:08:17 -0800

    Add Day 03 Example Value for Test

--- src/day-03/part-1/day-03-part-1.spec.ts
+++ src/day-03/part-1/day-03-part-1.spec.ts
@@ -4,7 +4,7 @@ import { SolveDay03Part1 } from './day-03-part-1'
 describe("Day 03 Part 1", () => {
   test("Example", async () => {
     const file = import.meta.resolveSync("./day-03-part-1-example.txt");
-    expect(await SolveDay03Part1(file)).toBe(123);
+    expect(await SolveDay03Part1(file)).toBe(4361);
   });
 
   test("Input", async () => {
