a3779eabe827c8cb23be4ec374ae71ee28d460fc
Cody Swartz <ctsstc@gmail.com>
2024-01-03 16:48:23 -0800

    Fix Day 04 Spec for File Location

--- src/day-04/utilities/day-04-card.spec.ts
+++ src/day-04/utilities/day-04-card.spec.ts
@@ -6,7 +6,9 @@ describe("Day 04 Part 1 > Utility - Card", async () => {
   const cardExample = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53";
   const card = day04ParseCard(cardExample);
 
-  const cardsExample = import.meta.resolveSync("../day-04-part-1-example.txt");
+  const cardsExample = import.meta.resolveSync(
+    "../part-1/day-04-part-1-example.txt"
+  );
   const cardsLines = await getLines(cardsExample);
   const cards = cardsLines.map((cardLine) => day04ParseCard(cardLine));
 
