ce7cca768cb03cbd962cda5ee75f6871bc26f4f2
Cody Swartz <ctsstc@gmail.com>
2024-01-06 02:18:23 -0800

    Update Day 03 Part 2 Test Example

--- src/day-03/day-03-part-2.spec.ts
+++ src/day-03/day-03-part-2.spec.ts
@@ -4,7 +4,7 @@ import { SolveDay03Part2 } from "./day-03-part-2";
 describe("Day 03 Part 2", () => {
   test("Example", async () => {
     const file = import.meta.resolveSync("./day-03-example.txt");
-    expect(await SolveDay03Part2(file)).toBe(123);
+    expect(await SolveDay03Part2(file)).toBe(467835);
   });
 
   test("Input", async () => {
