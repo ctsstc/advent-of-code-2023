79b87ec36b606b579de1d3a901637199eb8d19de
Cody Swartz <ctsstc@gmail.com>
2024-01-09 14:30:34 -0800

    Solve Day 04 Part 2
    Originally I was getting a huge number when utilizing the score rather than the count/length for a card. This is why I ended up utilizing BigInt, but now it's obvious that it was not required now that the actual solve is much smaller than I was originally getting 🤪

--- src/day-04/part-2/day-04-part-2.spec.ts
+++ src/day-04/part-2/day-04-part-2.spec.ts
@@ -4,13 +4,17 @@ import { SolveDay04Part2 } from "./day-04-part-2";
 describe("Day 04 Part 2", async () => {
   const exampleFile = import.meta.resolveSync("../day-04-example.txt");
   const day2SolveExample = await SolveDay04Part2(exampleFile);
+  const day2ExpectedExampleSolve = BigInt(30);
 
   test("Example", async () => {
-    expect(day2SolveExample).toBe(30);
+    expect(day2SolveExample).toBe(day2ExpectedExampleSolve);
   });
 
-  test.skipIf(day2SolveExample !== 30)("Input", async () => {
-    const file = import.meta.resolveSync("../day-04-input.txt");
-    expect(await SolveDay04Part2(file)).toBe(123);
-  });
+  test.skipIf(day2SolveExample !== day2ExpectedExampleSolve)(
+    "Input",
+    async () => {
+      const file = import.meta.resolveSync("../day-04-input.txt");
+      expect(await SolveDay04Part2(file)).toBe(BigInt(5704953));
+    }
+  );
 });
--- src/day-04/part-2/day-04-part-2.ts
+++ src/day-04/part-2/day-04-part-2.ts
@@ -1,8 +1,16 @@
 import { getLines } from "../../utilities/files";
+import { day04ParseCard } from "../utilities/day-04-card";
+import { day04ScoredCardsFromCards } from "../utilities/day-04-scored-card";
+import { day04ScoredCardAmounts } from "../utilities/day-04-scored-card-amounts";
 
-export async function SolveDay04Part2(filePath: string): Promise<number> {
-    const lines = await getLines(filePath);
+export async function SolveDay04Part2(filePath: string): Promise<BigInt> {
+  const lines = await getLines(filePath);
+  const cards = lines.map((line) => day04ParseCard(line));
+  const scoredCards = day04ScoredCardsFromCards(cards);
+  const cardAmounts = day04ScoredCardAmounts(scoredCards);
 
-    return 0;
+  return [...cardAmounts.values()].reduce(
+    (sum, current) => sum + current,
+    BigInt(0)
+  );
 }
-
