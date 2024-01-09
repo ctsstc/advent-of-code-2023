7961703501d266a53ed865c74bf445932923fe50
Cody Swartz <ctsstc@gmail.com>
2024-01-09 13:50:55 -0800

    Utilize New Day 04 Mocks in Scored Card Spec

--- src/day-04/utilities/day-04-scored-card.spec.ts
+++ src/day-04/utilities/day-04-scored-card.spec.ts
@@ -1,16 +1,9 @@
 import { describe, expect, it } from "bun:test";
-import { getLines } from "../../utilities/files";
-import { day04ParseCard } from "./day-04-card";
-import { day04ScoredCardsFromCards } from "./day-04-scored-card";
+import { mockDay04ScoredCards } from "./day-04.mocks";
 
 describe("ScoredCard", async () => {
-  const cardsExample = import.meta.resolveSync("../day-04-example.txt");
-  const cardsLines = await getLines(cardsExample);
-  const cards = cardsLines.map((cardLine) => day04ParseCard(cardLine));
-  const scoredCards = day04ScoredCardsFromCards(cards);
-
   it("returns the Scored Cards", () => {
-    const scores = scoredCards.map((scoredCard) => scoredCard.score);
+    const scores = mockDay04ScoredCards.map((scoredCard) => scoredCard.score);
 
     expect(scores).toEqual([8, 2, 2, 1, 0, 0]);
   });
