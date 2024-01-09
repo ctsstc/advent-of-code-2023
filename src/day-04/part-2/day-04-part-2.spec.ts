a703e3f0224cc31f4600c6873d11cdf968c2afaa
Cody Swartz <ctsstc@gmail.com>
2024-01-09 12:50:13 -0800

    Update Day 04 Part 2 Pathing for Input & Example
    This also utilizes a "skipIf" on the input file, so that those tests do not try to run until the example is solved. This helps reduce some noise in the test runner while watching.

--- src/day-04/part-2/day-04-part-2.spec.ts
+++ src/day-04/part-2/day-04-part-2.spec.ts
@@ -1,14 +1,16 @@
 import { describe, expect, test } from "bun:test";
 import { SolveDay04Part2 } from "./day-04-part-2";
 
-describe.skip("Day 04 Part 2", () => {
+describe("Day 04 Part 2", async () => {
+  const exampleFile = import.meta.resolveSync("../day-04-example.txt");
+  const day2SolveExample = await SolveDay04Part2(exampleFile);
+
   test("Example", async () => {
-    const file = import.meta.resolveSync("./day-04-part-2-example.txt");
-    expect(await SolveDay04Part2(file)).toBe(123);
+    expect(day2SolveExample).toBe(30);
   });
 
-  test("Input", async () => {
-    const file = import.meta.resolveSync("./day-04-part-2-input.txt");
+  test.skipIf(day2SolveExample !== 30)("Input", async () => {
+    const file = import.meta.resolveSync("../day-04-input.txt");
     expect(await SolveDay04Part2(file)).toBe(123);
   });
 });
