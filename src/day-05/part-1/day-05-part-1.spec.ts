37ba286b64cd1c17c56da1c48fb6c23aac253af9
Cody Swartz <ctsstc@gmail.com>
2024-01-10 01:06:35 -0800

    Add Day 05 Part 1 Example Answer

--- src/day-05/part-1/day-05-part-1.spec.ts
+++ src/day-05/part-1/day-05-part-1.spec.ts
@@ -1,10 +1,10 @@
-import { describe, expect, test } from 'bun:test'
-import { SolveDay05Part1 } from './day-05-part-1'
+import { describe, expect, test } from "bun:test";
+import { SolveDay05Part1 } from "./day-05-part-1";
 
 describe("Day 05 Part 1", async () => {
   const exampleFile = import.meta.resolveSync("../day-05-example.txt");
-  const solveExample = await SolveDay05Part1(exampleFile)
-  const expectedExample = 123;
+  const solveExample = await SolveDay05Part1(exampleFile);
+  const expectedExample = 35;
 
   test("Example", async () => {
     expect(solveExample).toBe(expectedExample);
