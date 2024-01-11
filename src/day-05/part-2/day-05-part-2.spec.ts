05e0327dcbd85e0d3571d3b4a579e15993af0281
Cody Swartz <ctsstc@gmail.com>
2024-01-10 23:16:17 -0800

    Update Day 05 Part 2 Example Spec

--- src/day-05/part-2/day-05-part-2.spec.ts
+++ src/day-05/part-2/day-05-part-2.spec.ts
@@ -1,10 +1,10 @@
-import { describe, expect, test } from 'bun:test'
-import { SolveDay05Part2 } from './day-05-part-2'
+import { describe, expect, test } from "bun:test";
+import { SolveDay05Part2 } from "./day-05-part-2";
 
 describe("Day 05 Part 2", async () => {
   const exampleFile = import.meta.resolveSync("../day-05-example.txt");
-  const solveExample = await SolveDay05Part2(exampleFile)
-  const expectedExample = 123;
+  const solveExample = await SolveDay05Part2(exampleFile);
+  const expectedExample = 46;
 
   test("Example", async () => {
     expect(solveExample).toBe(expectedExample);
