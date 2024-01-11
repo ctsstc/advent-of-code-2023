74803666577c48e7fffe0a5129f4b9f3eef0df7e
Cody Swartz <ctsstc@gmail.com>
2024-01-10 23:43:46 -0800

    Add Day 05 - Seeds Part 2
    This follows the new part 2 rules, and now with the new seeds we should be able to push those into our part 1 solving code.
    
    My concern is that the collection will likely be HUGE. At that point I'm not certain how to proceed if that is a problem. Given the large numbers I think the seed collection length is going to become very large. If it solves in a semi-reasonable time then I may disable this test after it solves. It's also possible something like dynamic programming or memoization is required here.

--- src/day-05/utilities/day-05-seeds.spec.ts
+++ src/day-05/utilities/day-05-seeds.spec.ts
@@ -1,10 +1,21 @@
 import { describe, expect, it } from "bun:test";
-import { day05Seeds } from "./day-05-seeds";
+import { day05Seeds, day05SeedsPart2 } from "./day-05-seeds";
+
+const firstLine = "seeds: 79 14 55 13";
 
 describe("Day 05 > Seeds", () => {
-  const firstLine = "seeds: 79 14 55 13";
-
   it("returns the seeds", () => {
     expect(day05Seeds(firstLine)).toEqual([79, 14, 55, 13]);
+  });
+});
+
+describe("Day 05 > Seeds Part 2", () => {
+  it("returns the seeds", () => {
+    expect(day05SeedsPart2(firstLine)).toEqual([
+      // range of: 14 values
+      79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92,
+      // range of: 13 values
+      55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67,
+    ]);
   });
 });
--- src/day-05/utilities/day-05-seeds.ts
+++ src/day-05/utilities/day-05-seeds.ts
@@ -4,3 +4,24 @@ export function day05Seeds(firstLine: string): number[] {
 
   return seedsStrings.map((seedsString) => parseInt(seedsString));
 }
+
+export function day05SeedsPart2(firstLine: string): number[] {
+  const part1Seeds = day05Seeds(firstLine);
+  const seeds: number[] = [];
+
+  for (let i = 0; i < part1Seeds.length; i += 2) {
+    const [start, range] = part1Seeds.slice(i, i + 2);
+
+    for (let ii = 0; ii < range; ii++) {
+      seeds.push(start + ii);
+    }
+  }
+
+  return seeds;
+}
+
+function day05SeedStrings(firstLine: string): number[] {
+  const seedsStrings = day05SeedStrings(firstLine);
+
+  return seedsStrings;
+}
